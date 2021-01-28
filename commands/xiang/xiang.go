4687a6c1b6e1f97622c4ee5495b2e01c9bf5f0c5
Max <shadow.iqka@qq.com>
2021-01-28 15:13:12 +0800

    + pack models

--- commands/xiang/xiang.go
+++ commands/xiang/xiang.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/gobuffalo/packr"
 	"github.com/yaoapp/yao/models/user"
 )
 
 func main() {
+	// pack model
+	packr.NewBox("../../models")
 	user := user.New()
 	user.Login()
 }
